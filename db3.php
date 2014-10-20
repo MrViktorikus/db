dc0e0fde2d89773cbc16916eb2273a30092e47ed
viktja710 <viktja710@E29632.edu.linkoping.se>
2014-10-20 10:56:14 +0200

    herrow

--- db3.php
+++ db3.php
@@ -5,25 +5,35 @@ define("DB_USER","root");
 define("DB_PASSWORD","");
 define("DB_NAME", "animal-house");
 
-$dbh = new PDO('mysql:dbname='.DB_NAME.';host='.DB_SERVER.';charset=utf8', DB_USER, DB_PASSWORD);
+//database-manager
+$dbm = new PDO('mysql:dbname='.DB_NAME.';host='.DB_SERVER.';charset=utf8', DB_USER, DB_PASSWORD);
 
 //urvalsfråga SQL
 $sql="SELECT * FROM cats";
+//$insert = "INSERT INTO cats(id, name) VALUES (id, 'Gandalf')";
+$update ="UPDATE cats SET id=[value-1],`name`=[value-2]";
+//$delete = "DELETE FROM cats";
 
 //statement
-$stmt = $dbh->prepare($sql);
+$stmt = $dbm->prepare($sql);
 $stmt->execute();
+//$stmt2 = $dbm->prepare($insert);
+//$stmt2->execute();
+$stmt3 = $dbm->prepare($update);
+$stmt3->execute();
+//$stmt4 = $dbm->prepare($delete);
+//$stmt4->execute();
 
 $cats = $stmt->fetchAll();
 
+//skriv ut array
 var_dump($cats);
 echo "<br>";
 
-//echo $cats[0]["name"];
+//skriv ut namnen i SUCH DATABASE
 foreach ($cats as $cat){
     echo $cat["name"];
     echo "<br>";
 }
-
 
 ?>
